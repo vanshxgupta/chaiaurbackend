1b7e3f35e92e7d9c1643a44fac95a5efbbec9b86
Vansh <vanshgupta987654321@gmail.com>
2025-01-28 23:11:06 +0530

    user controller and user routes guide

--- src/db/index.js
+++ src/db/index.js
@@ -7,7 +7,7 @@ const connectDB=async()=>{
         console.log(`\n MongoDB connected !! DB HOST:${connectionInstance.connection.host}}`)
        
     } catch (error) {
-        console.log("MONGODB connection FAILED",error);
+        console.log("MONGODB connection FAILED in file index.js in db",error);
         process.exit(1);
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 // import mongoose from "mongoose";
 // import  DB_NAME  from "./constants";
 import connectDB from "./db/index.js";
+import app from './app.js'
 
 dotenv.config({
     path:'./env'
@@ -23,7 +24,7 @@ connectDB()
 })
 
 .catch((err)=>{
-    console.log("MONGO db connection failed!!",err);
+    console.log("MONGO db connection failed in file index.js!!",err);
 })
 
 
