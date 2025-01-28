0a89dfdbd9707014aaf60f82263c4bb8082c0223
Vansh <vanshgupta987654321@gmail.com>
2025-01-28 14:54:32 +0530

    clodinary,multer ,fileupload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import {v2 as cloudinary} from "cloudinary"
+import { response } from "express";
+import fs from "fs"
+
+
+cloudinary.config({ 
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key:process.env.CLOUDINARY_API_KEY, 
+    api_secret:process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadonCloudinary=async (localFilePath)=>{
+
+    try {
+        if(!localFilePath) return null
+        //upload the file on cloudinary
+        const reponse=await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        //file has been uploaded successfully
+        console.log("file uploaded on cloudinary",response.url);
+        return response;
+
+    } 
+    catch (error) {
+        fs.unlinkSync(localFilePath) //remove the locally saved temporary file as the uploas operation got failed
+        return null;
+    }
+}
+
+export {uploadonCloudinary}
